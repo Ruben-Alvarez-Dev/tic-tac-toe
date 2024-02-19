61ed404de5b729d33876e1e61fcd07f03e9542b4
ruben <ruben@IMACPRO.local>
2024-02-19 18:24:34 +0100

    init project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+const TURNS = {
+  X: "x",
+  O: "o",
+};
+
+const board = Array(9).fill(null);
+
+function App() {
+  return (
+    <>
+      <h1>Hola</h1>
+    </>
+  );
+}
+
+export default App;
